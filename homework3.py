6982e66a97a7e94c59f552e4f7c6b6cabd6e8952
abuba0a <168736244+abuba0a@users.noreply.github.com>
2024-05-04 15:24:56 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,12 @@
+#1-е задание
+print('1-е задание')
+my_string = input('Введите название предмета: ')
+print(len(my_string))
+print( )
+#2-е задание
+print('2-е задание')
+print(my_string. upper())
+print(my_string. lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
